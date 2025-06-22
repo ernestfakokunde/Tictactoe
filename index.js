5326921466021e20fbca115fe883a2cbb9ccfdaa
Dev-ayo10 <ernest.dev10@gmail.com>
2025-06-22 18:37:13 +0100

    i created a new file for the index.js file and put all js there

--- index.js
+++ index.js
@@ -1,33 +1,85 @@
-let randomNumber = Math.random();
+
+let score ={
+  win:0,
+  losses:0,
+  ties:0
+}
+const scoreContainer = document.querySelector('.score-container');
+const displayMsg = document.querySelector('.display-msg');
+const restartButton = document.querySelector('.restart');
+
+
+function playGame(playerMove){
+
+const computerMove=PickComputerMove();
 let results = '';
 
-if(randomNumber >=0 && randomNumber < 0.5){
-  results = 'heads';
-}else if(randomNumber >= 0.5 && randomNumber < 1){
-  results = 'tails'
+if(playerMove === 'rock'){
+  if(computerMove=== 'rock'){
+    results = 'you tie'
+  }else if( computerMove === 'paper'){
+    results = 'you win'
+  }else if( computerMove === 'scissors'){
+    results = 'you lose'
+  }
 }
 
-let guess = results ;
-if(results === 'heads'){
-   console.log('You win')
-} else{
-  console.log('You lose')
+if(playerMove === 'paper'){
+  if(computerMove=== 'rock'){
+    results = 'you win'
+  }else if( computerMove === 'paper'){
+    results = 'you tie'
+  }else if( computerMove === 'scissors'){
+    results = 'you lose'
+  }
 }
 
-//lessson 7 functions
+if(playerMove === 'scissors'){
+  if(computerMove=== 'rock'){
+    results = 'you lose'
+  }else if( computerMove === 'paper'){
+    results = 'you win'
+  }else if( computerMove === 'scissors'){
+    results = 'you tie'
+  }
+}
+
+console.log(results);
+if(results === 'you win'){
+  score.win++;
+}else if(results === 'you lose'){
+  score.losses++;
+}else if(results === 'you tie'){
+  score.ties++;
+}
+console.log(score);
+displayMsg.innerHTML = `you choose <img src="${playerMove}.jpeg" alt=".." class="img"> computer choose <img src="${computerMove}.jpeg" alt=".." class="img">
+
+${results}`
+
+scoreContainer.innerHTML = `wins = ${score.win}, loses = ${score.losses}, ties = ${score.ties}`
+}
 
 
-// Document object models
+function PickComputerMove(){
+let randomNumber = Math.random();
+let computerMove = '';
 
-// we can also make specific keyboards key perform some functions
-//eg
-// we can say if event.key === 'Enter it should
-//perform some some task
+if(randomNumber >= 0 && randomNumber < 1/3 ){
+  computerMove = 'rock';
+}
+else if(randomNumber >= 1/3 && randomNumber < 2/3)
+{ computerMove = 'paper';}
+else if(randomNumber >= 2/3 && randomNumber < 1)
+{ computerMove = 'scissors';}
 
-// tp ake sure youre getting a value out of an input 
-// we should make sure we are using Number() an then the value in the brackets
+return computerMove;
+}
+restartButton.addEventListener('click', function(){
+  score.win = 0;
+  score.losses = 0;
+  score.ties = 0;
+  scoreContainer.innerHTML = `wins = ${score.win}, loses = ${score.losses}, ties = ${score.ties}`;
+  displayMsg.innerHTML = '';
+});
 
-// type corceion
-// if a string only contains a number and subsstracts multiply and divide it would convert that string into a number and perform the math operation
-// But if the math operation  is a + and a string is added to  a number it would convert the number into a string and then concatenate it or merge it together
-// best practices is to do maths using numbers not strings
